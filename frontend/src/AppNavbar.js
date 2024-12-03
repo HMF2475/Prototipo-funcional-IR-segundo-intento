f29ff5241d5105add3adf4f102bbb8fd4110946d
David <davidvt04@gmail.com>
2024-12-03 17:22:36 +0100

    Avances estéticos

--- frontend/src/AppNavbar.js
+++ frontend/src/AppNavbar.js
@@ -1,162 +1,84 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, NavbarBrand, NavLink, NavItem, Nav, NavbarText, NavbarToggler, Collapse } from 'reactstrap';
+import { 
+    Navbar, NavbarBrand, NavLink, NavItem, Nav, NavbarToggler, Collapse, Dropdown, DropdownToggle, DropdownMenu, DropdownItem 
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import tokenService from './services/token.service';
 import jwt_decode from "jwt-decode";
+import { CiSearch, CiUser } from "react-icons/ci";
 
 function AppNavbar() {
     const [roles, setRoles] = useState([]);
     const [username, setUsername] = useState("");
     const jwt = tokenService.getLocalAccessToken();
     const [collapsed, setCollapsed] = useState(true);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const toggleNavbar = () => setCollapsed(!collapsed);
+    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
     useEffect(() => {
         if (jwt) {
             setRoles(jwt_decode(jwt).authorities);
             setUsername(jwt_decode(jwt).sub);
         }
-    }, [jwt])
+    }, [jwt]);
 
-    let adminLinks = <></>;
-    let ownerLinks = <></>;
-    let userLinks = <></>;
-    let userLogout = <></>;
-    let publicLinks = <></>;
-
-    roles.forEach((role) => {
-        if (role === "ADMIN") {
-            adminLinks = (
-                <>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/owners">Owners</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/pets">Pets</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/vets">Vets</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/consultations">Consultations</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/clinicOwners">Clinic Owners</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/clinics">Clinics</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/users">Users</NavLink>
-                    </NavItem>
-                </>
-            )
-        }
-        if (role === "OWNER") {
-            ownerLinks = (
-                <>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/myPets">My Pets</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/consultations">Consultations</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/plan">Plan</NavLink>
-                    </NavItem>
-                </>
-            )
-        }
-        if (role === "VET") {
-            ownerLinks = (
-                <>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/consultations">Consultations</NavLink>
-                    </NavItem>
-                </>
-            )
-        }
-
-        if (role === "CLINIC_OWNER") {
-            ownerLinks = (
-                <>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/clinics">Clinics</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/owners">Owners</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/consultations">Consultations</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink style={{ color: "white" }} tag={Link} to="/vets">Vets</NavLink>
-                    </NavItem>
-                </>
-            )
-        }
-    })
-
-    if (!jwt) {
-        publicLinks = (
-            <>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="docs" tag={Link} to="/docs">Docs</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="plans" tag={Link} to="/plans">Pricing Plans</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="register" tag={Link} to="/register">Register</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="login" tag={Link} to="/login">Login</NavLink>
-                </NavItem>
-            </>
-        )
-    } else {
-        userLinks = (
-            <>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} tag={Link} to="/dashboard">Dashboard</NavLink>
-                </NavItem>
-            </>
-        )
-        userLogout = (
-            <>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="docs" tag={Link} to="/docs">Docs</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink style={{ color: "white" }} id="plans" tag={Link} to="/plans">Pricing Plans</NavLink>
-                </NavItem>
-                <NavbarText style={{ color: "white" }} className="justify-content-end">{username}</NavbarText>
-                <NavItem className="d-flex">
-                    <NavLink style={{ color: "white" }} id="logout" tag={Link} to="/logout">Logout</NavLink>
-                </NavItem>
-            </>
-        )
-
-    }
+    let publicLinks = (
+        <>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="TOTAL LOOK" tag={Link} to="/docs">TOTAL LOOK</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="A MEDIDA" tag={Link} to="/plans">A MEDIDA</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="COLECCIONES" tag={Link} to="/register">COLECCIONES</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="CATALOGOS" tag={Link} to="/register">CATALOGOS</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="KUBICO" tag={Link} to="/register">KUBICO</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="GRUPO BONO" tag={Link} to="/register">GRUPO BONO</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="ACTUALIDAD" tag={Link} to="/register">ACTUALIDAD</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink style={{ color: "white", fontFamily: 'Source Sans Pro' }} id="CONTACTO" tag={Link} to="/register">CONTACTO</NavLink>
+            </NavItem>
+            <div className="d-flex align-items-center" style={{ marginRight: '10px' }}>
+                <CiSearch style={{ color: 'white', fontSize: 30, cursor: 'pointer', marginRight: '15px' }} />
+                <Dropdown nav isOpen={dropdownOpen} toggle={toggleDropdown}>
+                    <DropdownToggle nav style={{ color: "white", cursor: 'pointer' }}>
+                        <CiUser style={{ fontSize: 30 }} />
+                    </DropdownToggle>
+                    <DropdownMenu end>
+                        <DropdownItem tag={Link} to="/login">Iniciar sesión</DropdownItem>
+                        <DropdownItem tag={Link} to="/register">Registrarse</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </div>
+        </>
+    );
 
     return (
         <div>
-            <Navbar expand="md" dark color="dark">
+            <Navbar expand="md" style={{ backgroundColor: 'rgb(77,86,80)' }}>
                 <NavbarBrand href="/">
-                    <img alt="logo" src="/logo1-recortado.png" style={{ height: 40, width: 40 }} />
-                    PetClinic
+                    <img 
+                        alt="logo" 
+                        src="https://kubicoarmarios.com/wp-content/uploads/thegem-logos/logo_5ca063ef8f9677b5335b1f6a7ffd00a1_2x.png" 
+                        style={{ height: 80, width: 150, marginLeft: 80 }} 
+                    />
                 </NavbarBrand>
                 <NavbarToggler onClick={toggleNavbar} className="ms-2" />
                 <Collapse isOpen={!collapsed} navbar>
-                    <Nav className="me-auto mb-2 mb-lg-0" navbar>
-                        {userLinks}
-                        {adminLinks}
-                        {ownerLinks}
-                    </Nav>
                     <Nav className="ms-auto mb-2 mb-lg-0" navbar>
                         {publicLinks}
-                        {userLogout}
                     </Nav>
                 </Collapse>
             </Navbar>
--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
+import tokenService from '../services/token.service.js';
+import { RiQuestionnaireLine } from "react-icons/ri";
 
-export default function Home(){
-    return(
+export default function Home() {
+    const usuario = tokenService.getUser();
+
+    return (
         <div className="home-page-container">
-            <div className="hero-div">
-                <h1>Petclinic</h1>
-                <h3>---</h3>
-                <h3>Find the best vet for your pet</h3>                
-            </div>
+            {usuario.roles !== 'CAMBIAR' &&
+                <div style={{
+                    position: 'fixed',  // Posiciona el ícono respecto a la ventana
+                    bottom: '30px',     // Coloca el ícono 30px arriba del borde inferior
+                    right: '30px',      // Coloca el ícono 30px a la izquierda del borde derecho
+                    fontSize: '60px',    // Tamaño del ícono
+                    cursor: 'pointer',  // Cambia el cursor a mano
+                    zIndex: 9999        // Asegura que el ícono esté encima de otros elementos
+                }}>
+                    <RiQuestionnaireLine style={{ color: 'red' }} />
+                </div>
+            }
         </div>
     );
 }
+
+
